24305b66f09079c25fd24a8af2fb5128aeba4b96
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-08-08 18:11:18 +0200

    Update to latest api

---
+++ sdk/gen/src/mt_2025_01_01_dashboard/endpoints/provider_oauth.py
@@ -0,0 +1,22 @@
+from metorial_util_endpoint import BaseMetorialEndpoint, MetorialEndpointManager, MetorialRequest
+from ..resources import mapProviderOauthDiscoverOutput, ProviderOauthDiscoverOutput, mapProviderOauthDiscoverBody, ProviderOauthDiscoverBody
+
+class MetorialProviderOauthEndpoint(BaseMetorialEndpoint):
+    """Get OAuth connection template information"""
+
+    def __init__(self, config: MetorialEndpointManager):
+        super().__init__(config)
+
+    def discover(self, body: ProviderOauthDiscoverBody):
+        """
+    Discover OAuth Configuration
+    Discover OAuth configuration from a discovery URL
+
+    :param body: ProviderOauthDiscoverBody
+    :return: ProviderOauthDiscoverOutput
+    """
+        request = MetorialRequest(
+            path=['provider-oauth-discovery'],
+            body=mapProviderOauthDiscoverBody.to_dict(body),
+        )
+        return self._post(request).transform(mapProviderOauthDiscoverOutput.from_dict)
